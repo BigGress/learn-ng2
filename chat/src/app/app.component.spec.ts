b08e42f6cab7f5ec8da330700c35b5fd5c1c6ebc
gress <451148156@qq.com>
2017-05-18 22:26:11 +0800

    feat(测试): 给chat应用添加dom测试

--- chat/src/app/app.component.spec.ts
+++ chat/src/app/app.component.spec.ts
@@ -1,4 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { MessageService, UserService, DataService } from "../services";
 
 import { AppComponent } from './app.component';
 import { BarComponent } from "./components/chatBar/chatBar.component";
@@ -14,25 +19,35 @@ describe('AppComponent', () => {
         ChatUserComponent,
         ChatWindowComponent,
       ],
+      imports: [
+        FormsModule,
+        BrowserModule,
+        HttpModule,
+      ],
+      providers: [
+        MessageService,
+        UserService,
+        DataService,
+      ]
     }).compileComponents();
   }));
 
-  // it('should create the app', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 
-  // it(`should have as title 'app works!'`, async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('app works!');
-  // }));
+  it(`should have as title 'app works!'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app works!');
+  }));
 
-  // it('should render title in a h1 tag', async(() => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  // }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    // expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  }));
 });
